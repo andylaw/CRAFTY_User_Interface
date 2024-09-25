27b2e31ae3241d617ba53b46a27789c3f91ed407
byari-m <byari-m@IMK-IFU-L2634.ad.imk-ifu.kit.edu>
2024-09-25 14:15:09 +0200

    push test

--- Crafty_UserInterface/src/main/java/main/FxMain.java
+++ Crafty_UserInterface/src/main/java/main/FxMain.java
@@ -41,7 +41,7 @@ public class FxMain extends Application {
 	private static final Logger LOGGER = LogManager.getLogger(FxMain.class);
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		LOGGER.info(/* "\u001B[33m"+ */ "--Starting with CRAFTY--" /* +"\u001B[0m" */);		
+		LOGGER.info(/* "\u001B[33m"+ */"--Starting with CRAFTY--"/* +"\u001B[0m" */);		
 		
 		double w = Screen.getPrimary().getBounds().getWidth();
 		double h = Screen.getPrimary().getBounds().getHeight();
