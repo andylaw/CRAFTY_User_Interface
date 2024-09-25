3c5a2d4bd413ea05daf6a3aee4b215e49d93a401
byari-m <byari-m@IMK-IFU-L2634.ad.imk-ifu.kit.edu>
2024-09-25 12:15:01 +0200

    test Commit, Push, YMAL

--- Crafty_UserInterface/src/test/java/main/FxMainTest.java
+++ Crafty_UserInterface/src/test/java/main/FxMainTest.java
@@ -1,29 +1,15 @@
 package main;
 
-import javafx.application.Platform;
-import javafx.stage.Stage;
 
-import static org.junit.Assert.assertTrue;
 
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.testfx.framework.junit5.ApplicationExtension;
-import org.testfx.framework.junit5.ApplicationTest;
 
-@ExtendWith(ApplicationExtension.class)
-public class FxMainTest extends ApplicationTest {
+public class FxMainTest  {
 
 
 	@Test
 	public void testStageShows() throws InterruptedException {
-		try {
-			new FxMain().start(FxMain.primaryStage);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		System.out.println("test");
 }
 }
