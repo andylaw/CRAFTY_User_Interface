65084a0f5e1bc6b5f4b1b6ead8023a8486424a12
byari-m <byari-m@IMK-IFU-L2634.ad.imk-ifu.kit.edu>
2024-11-12 10:28:43 +0100

    Implement non-GUI CRAFTY run, add price and service demand, update

--- Crafty_UserInterface/src/main/java/main/Config.java
+++ Crafty_UserInterface/src/main/java/main/Config.java
@@ -1,34 +1,30 @@
 package main;
 
-import UtilitiesFx.filesTools.PathTools;
-
 public class Config {
 
-	public String project_path = PathTools.asFolder("mnt") + PathTools.asFolder("c")  + PathTools.asFolder("Users") + PathTools.asFolder("byari-m")
-			+ PathTools.asFolder("Desktop") + PathTools.asFolder("CRAFTY-Cobra-Code-compareson")
-			+ PathTools.asFolder("data_for_UI");
-	public String scenario = "RCP2_6-SSP4";
-	public boolean regionalization = false;
+	public String project_path;
+	public String scenario;
+	public boolean regionalization;
 	// CRAFTY Mechanisms
-	public boolean initial_demand_supply_equilibrium = true;
-	public boolean remove_negative_marginal_utility = false;
-	public boolean use_abandonment_threshold = true;
-	public boolean mutate_on_competition_win = false;
-	public double mutation_interval = 0.01;
-	public double MostCompetitorAFTProbability = 0.8;
-	public boolean averaged_residual_demand_per_cell = false;
+	public boolean initial_demand_supply_equilibrium;
+	public boolean remove_negative_marginal_utility;
+	public boolean use_abandonment_threshold;
+	public boolean mutate_on_competition_win;
+	public double mutation_interval;
+	public double MostCompetitorAFTProbability;
+	public boolean averaged_residual_demand_per_cell;
 	// Neighboring Effects
-	public boolean use_neighbor_priority = true;
-	public double neighbor_priority_probability = 0.95;
-	public int neighbor_radius = 2;
+	public boolean use_neighbor_priority;
+	public double neighbor_priority_probability;
+	public int neighbor_radius;
 	// Competitiveness Process
-	public double participating_cells_percentage = 0.03;
-	public int marginal_utility_calculations_per_tick = 10;
-	public double land_abandonment_percentage = 0.05;
+	public double participating_cells_percentage;
+	public int marginal_utility_calculations_per_tick;
+	public double land_abandonment_percentage;
 	// Output Configurati
-	public String output_folder_name = "linux";
-	public boolean generate_csv_files = true;
-	public int csv_output_frequency = 50;
-	public boolean track_changes = false;
+	public String output_folder_name;
+	public boolean generate_csv_files;
+	public int csv_output_frequency;
+	public boolean track_changes;
 
 }
--- Crafty_UserInterface/src/main/java/main/MainHeadless.java
+++ Crafty_UserInterface/src/main/java/main/MainHeadless.java
@@ -18,16 +18,14 @@ import model.ModelRunner;
 public class MainHeadless {
 	private static final Logger LOGGER = LogManager.getLogger(MainHeadless.class);
 
-
 	public static void main(String[] args) {
 		LOGGER.info(/* "\u001B[33m"+ */"--Starting runing CRAFTY--"/* +"\u001B[0m" */);
-		 dataImport();
-		 run();
-		System.out.println("Hello from Java!");
+		modelInitialisation();
+		run();
 	}
 
-	static void dataImport() {
-		System.out.println("is linux:  "+ System.getProperty("os.name").toLowerCase().contains("linux"));
+	static void modelInitialisation() {
+		System.out.println("is linux:  " + System.getProperty("os.name").toLowerCase().contains("linux"));
 		PathsLoader.initialisation(Paths.get(ConfigLoader.config.project_path));
 		PathsLoader.setScenario(ConfigLoader.config.scenario);
 		CellsLoader.loadCapitalsList();
@@ -42,6 +40,7 @@ public class MainHeadless {
 		ModelRunnerController.tick = new AtomicInteger(PathsLoader.getStartYear());
 		ModelRunnerController.outputfolderPath(ConfigLoader.config.output_folder_name);
 		ModelRunnerController.demandEquilibrium();
+		
 		for (int i = 0; i <= PathsLoader.getEndtYear() - PathsLoader.getStartYear(); i++) {
 			PathsLoader.setCurrentYear(ModelRunnerController.tick.get());
 			System.out.println("-------------   " + PathsLoader.getCurrentYear() + "   --------------");
