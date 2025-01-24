91cb4dc7ca74833dd2fdf65f9b41ab47affd2310
Andy Law <andy.law@roslin.ed.ac.uk>
2025-01-24 16:12:50 +0000

    Added a CraftyOptions object

---
+++ Crafty_UserInterface/src/main/java/main/CraftyOptions.java
@@ -0,0 +1,16 @@
+package main;
+
+public class CraftyOptions {
+
+    public static final String CONFIG_FILE_PATH_OPTION = "config-file-path";
+
+    private String configFilePath;
+
+    public String getConfigFilePath() {
+        return configFilePath;
+    }
+
+    public void setConfigFilePath(String configFilePath) {
+        this.configFilePath = configFilePath;
+    }
+}
