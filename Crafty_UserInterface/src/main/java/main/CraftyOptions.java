cb91e540fc43353661ee2588790a80571c6f0e0e
Andy Law <andy.law@roslin.ed.ac.uk>
2025-01-25 11:01:13 +0000

    Extend the CraftyOptions object to separate out the projectDirectoryPath

--- Crafty_UserInterface/src/main/java/main/CraftyOptions.java
+++ Crafty_UserInterface/src/main/java/main/CraftyOptions.java
@@ -3,8 +3,10 @@ package main;
 public class CraftyOptions {
 
     public static final String CONFIG_FILE_PATH_OPTION = "config-file-path";
+    public static final String PROJECT_DIRECTORY_PATH_OPTION = "project-directory-path";
 
     private String configFilePath;
+    private String projectDirectoryPath;
 
     public String getConfigFilePath() {
         return configFilePath;
@@ -12,5 +14,13 @@ public class CraftyOptions {
 
     public void setConfigFilePath(String configFilePath) {
         this.configFilePath = configFilePath;
+    }
+
+    public String getProjectDirectoryPath() {
+        return projectDirectoryPath;
+    }
+
+    public void setProjectDirectoryPath(String projectDirectoryPath) {
+        this.projectDirectoryPath = projectDirectoryPath;
     }
 }
